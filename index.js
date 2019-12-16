7a8fe4c45bd94cc79eec27caba57e4844e4a412b
Samuel Meuli <me@samuelmeuli.com>
2019-12-16 19:46:16 +0100

    Check for required input variables

--- index.js
+++ index.js
@@ -11,17 +11,47 @@ const MAVEN_SETTINGS_PATH = path.join(__dirname, "settings.xml");
 const log = msg => console.log(`\n${msg}`); // eslint-disable-line no-console
 
 /**
+ * Exits the current process with an error code and message
+ */
+const exit = msg => {
+	console.error(msg);
+	process.exit(1);
+};
+
+/**
  * Executes the provided shell command and redirects stdout/stderr to the console
  */
 const run = cmd => execSync(cmd, { encoding: "utf8", stdio: "inherit" });
 
 /**
+ * Returns the value for an environment variable (or `null` if it's not defined)
+ */
+const getEnv = name => process.env[name.toUpperCase()] || null;
+
+/**
+ * Returns the value for an input variable (or `null` if it's not defined). If the variable is
+ * required and doesn't have a value, abort the action
+ */
+const getInput = (name, required) => {
+	const value = getEnv(`INPUT_${name}`);
+	if (required && !value) {
+		exit(`"${name}" input variable is not defined`);
+	}
+	return value;
+};
+
+/**
  * Runs the deployment
  */
 const runAction = () => {
+	// Make sure the required input variables are provided
+	getInput("gpg_passphrase", true);
+	getInput("nexus_username", true);
+	getInput("nexus_password", true);
+
 	// Import GPG key into keychain
 	log("Importing GPG key…");
-	writeFileSync(GPG_KEY_PATH, process.env.INPUT_GPG_PRIVATE_KEY);
+	writeFileSync(GPG_KEY_PATH, getInput("gpg_private_key", true));
 	run(`gpg --import --batch ${GPG_KEY_PATH}`);
 	unlinkSync(GPG_KEY_PATH);
 
